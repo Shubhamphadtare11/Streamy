13d978a5acc27b645e6dc67621b56c3b7158eb3a
Shubham <shubham.phadtarez11@gmail.com>
2024-01-21 13:01:58 +0530

    minor changes 4

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ const GOOGLE_API_KEY = "AIzaSyBuVN1tc1pexZqV014kwX-LYe3HE-0fOqI";
 
 export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + GOOGLE_API_KEY;
 
-export const YOUTUBE_SEARCH_API = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+export const YOUTUBE_SEARCH_API = "https://thingproxy.freeboard.io/fetch/https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const YOUTUBE_VIDEO_BYID = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&key=${GOOGLE_API_KEY}&id=`;
 
