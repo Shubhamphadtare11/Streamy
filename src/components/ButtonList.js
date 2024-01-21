735ed18775222817d03039f7c47514bfb895eab6
Shubham <shubham.phadtarez11@gmail.com>
2024-01-21 14:22:54 +0530

    minor change

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -13,13 +13,18 @@ const ButtonList = () => {
     "Soccer",
     "WWE",
     "Valentine",
+    "Tennis",
+    "Entertainment",
+    "Bollywood",
   ];
 
   return (
-    <div className="flex flex-wrap">
+    <div className="overflow-x-scroll sm:overflow-x-auto w-80 sm:w-full">
+    <div className="flex sm:flex-wrap">
       {listButtons.map((list, index) => (
         <Button name={list} key={index} />
       ))}
+    </div>
     </div>
   );
 };
